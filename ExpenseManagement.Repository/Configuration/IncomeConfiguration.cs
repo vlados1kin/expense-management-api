42631f5b7a5d1172d0a0122a0a777f710493ec53
Vladislav Lashkin <lashkin2004@gmail.com>
2024-08-09 19:03:30 +0300

    create configuration files for domain entities

---
+++ ExpenseManagement.Repository/Configuration/IncomeConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using ExpenseManagement.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ExpenseManagement.Repository.Configuration;
+
+public class IncomeConfiguration : IEntityTypeConfiguration<Income>
+{
+    public void Configure(EntityTypeBuilder<Income> builder)
+    {
+        builder.HasKey(i => i.Id);
+
+        builder.HasOne(i => i.User)
+            .WithMany(u => u.Incomes)
+            .HasForeignKey(i => i.UserId)
+            .OnDelete(DeleteBehavior.Cascade);
+        
+        builder.Property(i => i.Amount).HasColumnType("DECIMAL(18, 2)");
+
+        builder.Property(i => i.Source).HasMaxLength(100).IsRequired();
+
+        builder.Property(i => i.CreatedAt).HasDefaultValueSql("GETDATE()");
+    }
+}
---
+++ ExpenseManagement.Repository/Configuration/UserConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using ExpenseManagement.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ExpenseManagement.Repository.Configuration;
+
+public class UserConfiguration : IEntityTypeConfiguration<User>
+{
+    public void Configure(EntityTypeBuilder<User> builder)
+    {
+        builder.HasKey(u => u.Id);
+
+        builder.HasOne(u => u.Balance)
+            .WithOne(b => b.User)
+            .HasForeignKey<Balance>(b => b.UserId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        builder.HasIndex(u => u.Username).IsUnique();
+        builder.Property(u => u.Username).HasMaxLength(100).IsRequired();
+        
+        builder.HasIndex(u => u.Email).IsUnique();
+        builder.Property(u => u.Email).HasMaxLength(100).IsRequired();
+        
+        builder.Property(u => u.PasswordHash).HasMaxLength(255).IsRequired();
+
+        builder.Property(u => u.CreatedAt).HasDefaultValueSql("GETDATE()");
+    }
+}
