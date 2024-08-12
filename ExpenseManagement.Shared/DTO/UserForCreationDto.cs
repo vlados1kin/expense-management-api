7f01f72c4ace036d3fe7c6fe33549201f156a171
Vladislav Lashkin <lashkin2004@gmail.com>
2024-08-12 22:22:21 +0300

    create user for creation dto

---
+++ ExpenseManagement.Shared/DTO/UserForCreationDto.cs
@@ -0,0 +1,8 @@
+﻿namespace ExpenseManagement.Shared.DTO;
+
+public record UserForCreationDto
+{
+    public string? Username { get; init; }
+    public string? Email { get; init; }
+    public string? PasswordHash { get; init; }
+}
