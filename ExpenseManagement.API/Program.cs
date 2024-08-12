2cc1900d4d6dcf1a3c02d54a833d457d50f055c9
Vladislav Lashkin <lashkin2004@gmail.com>
2024-08-12 22:21:53 +0300

    apply configuration method in the service collection

--- ExpenseManagement.API/Program.cs
+++ ExpenseManagement.API/Program.cs
@@ -1,3 +1,5 @@
+using ExpenseManagement.API.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,6 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.ConfigureSqlContext(builder.Configuration);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
