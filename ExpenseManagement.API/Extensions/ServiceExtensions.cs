3288d710c76c7bdedcd1ce03a66f219ae1a70945
Vladislav Lashkin <lashkin2004@gmail.com>
2024-08-12 22:20:40 +0300

    add configure sql context method in the service extensions file

---
+++ ExpenseManagement.API/Extensions/ServiceExtensions.cs
@@ -0,0 +1,11 @@
+﻿using ExpenseManagement.Repository;
+using Microsoft.EntityFrameworkCore;
+
+namespace ExpenseManagement.API.Extensions;
+
+public static class ServiceExtensions
+{
+    public static void ConfigureSqlContext(this IServiceCollection services, IConfiguration configuration)
+        => services.AddDbContext<RepositoryContext>(options =>
+            options.UseSqlServer(configuration.GetConnectionString("sqlConnection")));
+}
